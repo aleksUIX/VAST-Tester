74f531e92cff647d1f577ee2c31e436ffd36f87c
Aleksander Sekowski <aleksander@uixlimited.com>
2026-05-24 14:33:38 -0700

    Deduplicate React in Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
   plugins: [react(), wasm(), topLevelAwait()],
+  resolve: {
+    dedupe: ["react", "react-dom"],
+  },
   server: {
     port: 5174,
   },
